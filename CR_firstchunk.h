f301302be3a6bc95fc3cae3de610b3f863fa8b55
Thabongshot <tblee@cyrusian.com>
2012-03-29 12:32:53 -0700

    Divided modules

---
+++ CR_firstchunk.h
@@ -0,0 +1,13 @@
+/*
+ * Web Crawler.
+ * Working functions for first chunk.
+ * 
+ * arg : 
+ * >> get chunk string ( use getchunkbody module )
+ * 	>> hyperlink filtering
+ * 	>> title string filtering
+ * 	>> make them into one set & save set to memory
+ * >>return pointer
+ *
+ */
+
---
+++ CR_main.c
@@ -0,0 +1,5 @@
+/*
+	Web Crawler main body file.
+	
+*/
+
